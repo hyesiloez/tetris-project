a69950ccc62b236450748f56a9269da4431dbc09
jonas.plankert <jonas.plankert@stud.uni-hannover.de>
2023-06-25 22:04:45 +0200

    test GUI

--- GUI/src/main/java/com/example/gui/MenuApplication.java
+++ GUI/src/main/java/com/example/gui/MenuApplication.java
@@ -1,20 +1,52 @@
 package com.example.gui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MenuApplication extends Application {
+public class MenuApplication extends Application  {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("hello-gui.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-gui.fxml"));
+            Parent root = loader.load();
+            MenuController controller = loader.getController();
+            Scene scene = new Scene(root, 1280, 720);
+
+            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent keyEvent) {
+
+                    switch (keyEvent.getCode()) {
+                        case ESCAPE:
+                            try {
+                                controller.back();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+            });
+
+            stage.setTitle("Tetris");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- GUI/src/main/java/com/example/gui/MenuController.java
+++ GUI/src/main/java/com/example/gui/MenuController.java
@@ -1,14 +1,92 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void singleplayer(ActionEvent e) throws IOException {
+        System.out.println("Singleplayer");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("singleplayer.fxml"));
+        root = loader.load();
+        MenuController controller = loader.getController();
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+
+                switch (keyEvent.getCode()) {
+                    case ESCAPE:
+                        try {
+                            controller.back();
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        });
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void multiplayer(ActionEvent e) throws IOException {
+        System.out.println("Multiplayer");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("multiplayer.fxml"));
+        root = loader.load();
+        MenuController controller = loader.getController();
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+
+                switch (keyEvent.getCode()) {
+                    case ESCAPE:
+                        try {
+                            controller.back();
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void exit(ActionEvent e) {
+        System.out.println("Exit");
+    }
+
+    public void back() throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-gui.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
